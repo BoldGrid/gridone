dedace81552f0764ed5c9429261b8cbe2c7b7eac
Tim <dev@tim.ph>
2017-02-01 12:07:33 -0500

    removing disabled state from configs

--- inc/boldgrid-theme-framework-config/config.php
+++ inc/boldgrid-theme-framework-config/config.php
@@ -15,8 +15,6 @@ function boldgrid_theme_framework_config( $boldgrid_framework_configs ) {
 	$boldgrid_framework_configs['boldgrid-parent-theme'] = true;
 	// Specify the parent theme's name.
 	$boldgrid_framework_configs['parent-theme-name'] = 'prime';
-	// Disable call to action widgets for this theme.
-	$boldgrid_framework_configs['template']['call-to-action'] = 'disabled';
 	// Select the footer template to use.
 	$boldgrid_framework_configs['template']['footer'] = 'generic';
 	// Select the header template to use.
@@ -35,7 +33,7 @@ function boldgrid_theme_framework_config( $boldgrid_framework_configs ) {
 	// Assign Locations for Generic Footer.
 	$boldgrid_framework_configs['template']['locations']['footer'] = array(
 		'1' => array( '[menu]footer_center' ),
-		'5' => array( '[widget]boldgrid-widget-3', '[action]boldgrid_display_contact_block' ),
+		'5' => array( '[widget]boldgrid-widget-3' ),
 		'8' => array( '[action]boldgrid_display_attribution_links' ),
 	);
 
